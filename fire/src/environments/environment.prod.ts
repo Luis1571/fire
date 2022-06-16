e5a9e6df0e8837e57d39e1e30f7d3e62a7a31b8e
Luis1571 <55500525+Luis1571@users.noreply.github.com>
2022-06-16 18:15:23 +0200

    commit inti prueba
    Proyecto del Hosting firebase
    info de https://www.youtube.com/watch?v=GwWIlMKUCJo

---
+++ fire/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDkmq_lyiwTmHtg9KFecj3_Zirj15id_PY",
+    authDomain: "udemy-tarjeta.firebaseapp.com",
+    databaseURL:
+      "https://udemy-tarjeta-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "udemy-tarjeta",
+    storageBucket: "udemy-tarjeta.appspot.com",
+    messagingSenderId: "665332438625",
+    appId: "1:665332438625:web:0acb17974f389145bebc20",
+  },
+};
---
+++ fire/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDkmq_lyiwTmHtg9KFecj3_Zirj15id_PY",
+    authDomain: "udemy-tarjeta.firebaseapp.com",
+    databaseURL:
+      "https://udemy-tarjeta-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "udemy-tarjeta",
+    storageBucket: "udemy-tarjeta.appspot.com",
+    messagingSenderId: "665332438625",
+    appId: "1:665332438625:web:0acb17974f389145bebc20",
+  },
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
